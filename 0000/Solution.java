fe6e95f3df708b2b72c67f9d6f4a659c7437478a
Luzi82 <luzi82@gmail.com>
2019-04-28 14:31:40 +0800

    add sample

--- 0000/Solution.java
+++ 0000/Solution.java
@@ -7,7 +7,7 @@ class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-        int t = in.nextInt(); // Scanner has functions to read ints, longs, strings, chars, etc.
+        int t = in.nextInt();
         System.out.println("asdf");
     }
     
